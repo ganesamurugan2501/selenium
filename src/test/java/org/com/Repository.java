b15262bdea3f79f45a75c910e4021378d6672d91
ganesamurugan2501 <ganeshm2501@gmail.com>
2021-11-12 20:16:28 +0530

    text

---
+++ src/test/java/org/com/Repository.java
@@ -0,0 +1,12 @@
+package org.com;
+
+public class Repository {
+private void method11() {
+	// TODO Auto-generated method stub
+
+}
+private void method12() {
+	// TODO Auto-generated method stub
+
+}
+}
